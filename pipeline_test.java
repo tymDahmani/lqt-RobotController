a8017a65e40b577d0c9cd61aac12a914814d2a62
tay_ma_alda <temyhey@gmail.com>
2023-12-25 23:45:36 +0100

    idk wt to write here but this should be the last push

--- pipeline_test.java
+++ pipeline_test.java
@@ -1,85 +1,61 @@
 package org.firstinspires.ftc.teamcode.openCV__autoDetect;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 import static java.lang.Thread.sleep;
 
-import android.os.Environment;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
-import org.openftc.easyopencv.OpenCvWebcam;
-import org.openftc.easyopencv.PipelineRecordingParameters;
 
 
-@Autonomous
-public class eocv_test_red extends OpMode {
-
-    OpenCvWebcam webCam1 = null;
-
-    @Override
-    public void init() {
-
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webCam1");
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-
-        webCam1.setPipeline(new pipeline());
-
-        webCam1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                /* input the camera stream details
-                // The first two parameters are the desired width and height of the image stream, which are one of these:
-                // 320x240
-                // 640x480
-                // 1280x720
-                // 1920x1080
-                The third parameter specifies the orientation the camera is being used in. So for instance,
-                if you have a phone mounted in portrait on your robot, or a webcam used in its normal orientation, use UPRIGHT.
-                But if you have the phone mounted in landscape on your robot, use SIDEWAYS_LEFT or SIDEWAYS_RIGHT depending on the specific way you've mounted it.
-                SIDEWAYS_LEFT refers to the orientation where the screen is facing toward you in landscape mode and the USB port is facing to the right.
-
-                 example: camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-                 */
-                webCam1.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-
-            }
-        });
-    }
-
-
-    @Override
-    public void loop() {
-        /*
-         * Send some stats to the telemetry
-         */
-        telemetry.addData("Frame Count", webCam1.getFrameCount());
-        telemetry.addData("FPS", String.format("%.2f", webCam1.getFps()));
-        telemetry.addData("Total frame time ms", webCam1.getTotalFrameTimeMs());
-        telemetry.addData("Pipeline time ms", webCam1.getPipelineTimeMs());
-        telemetry.addData("Overhead time ms", webCam1.getOverheadTimeMs());
-        telemetry.addData("Theoretical max FPS", webCam1.getCurrentPipelineMaxFps());
-        telemetry.update();
+//@Autonomous
+//public class eocv_test_red extends OpMode {
+//
+//    OpenCvWebcam webCam1 = null;
+//
+//    @Override
+//    public void init() {
+//
+//        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webCam1");
+//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+//        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+//
+//        webCam1.setPipeline(new pipeline());
+//
+//        webCam1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+//            @Override
+//            public void onOpened() {
+//                /* input the camera stream details
+//                // The first two parameters are the desired width and height of the image stream, which are one of these:
+//                // 320x240
+//                // 640x480
+//                // 1280x720
+//                // 1920x1080
+//                The third parameter specifies the orientation the camera is being used in. So for instance,
+//                if you have a phone mounted in portrait on your robot, or a webcam used in its normal orientation, use UPRIGHT.
+//                But if you have the phone mounted in landscape on your robot, use SIDEWAYS_LEFT or SIDEWAYS_RIGHT depending on the specific way you've mounted it.
+//                SIDEWAYS_LEFT refers to the orientation where the screen is facing toward you in landscape mode and the USB port is facing to the right.
+//
+//                 example: camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+//                 */
+//                webCam1.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
+//            }
+//
+//            @Override
+//            public void onError(int errorCode) {
+//
+//            }
+//        });
+//    }
 
 
-    }
+    public class pipeline_test extends OpenCvPipeline {
 
-    class pipeline extends OpenCvPipeline {
+        // our camera stream image:
+        // webCam1.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
 
         Mat red = new Mat();
 
@@ -97,7 +73,7 @@ public class eocv_test_red extends OpMode {
         // an index to store the position of the team prop (1; left, 2; mid, 3; right)
         int pos_of_tp = 0;
 
-
+        @Override
         public Mat processFrame(Mat input) {
             // Executed every time a new frame is dispatched
 
@@ -199,7 +175,7 @@ public class eocv_test_red extends OpMode {
 //
 //        }
     }
-}
+
 
 
 ////////////////taymaaaa iz da bezt programmer
