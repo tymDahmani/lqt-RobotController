2f83a234f3d668cdbff0fa5a86825c50fd5a4e33
tay_ma_alda <temyhey@gmail.com>
2023-12-25 18:56:11 +0100

    but my brain is a silicone mimic of a dom ;-;

--- eocv_test_tyma.java
+++ eocv_test_tyma.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.openCV__autoDetect;
 
 import static org.openftc.easyopencv.OpenCvCameraRotation.UPRIGHT;
 
+import static java.lang.Thread.sleep;
+
+import android.os.Environment;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -12,12 +16,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+import org.openftc.easyopencv.PipelineRecordingParameters;
 
 
 @Autonomous
@@ -60,14 +67,28 @@ public class eocv_test_tyma extends OpMode {
         });
     }
 
+
     @Override
     public void loop() {
+        /*
+         * Send some stats to the telemetry
+         */
+        telemetry.addData("Frame Count", webCam1.getFrameCount());
+        telemetry.addData("FPS", String.format("%.2f", webCam1.getFps()));
+        telemetry.addData("Total frame time ms", webCam1.getTotalFrameTimeMs());
+        telemetry.addData("Pipeline time ms", webCam1.getPipelineTimeMs());
+        telemetry.addData("Overhead time ms", webCam1.getOverheadTimeMs());
+        telemetry.addData("Theoretical max FPS", webCam1.getCurrentPipelineMaxFps());
+        telemetry.update();
+
 
     }
 
     class pipeline extends OpenCvPipeline {
 
         Mat grey = new Mat();
+
+        boolean toggleRecording = false;
 
 
 
@@ -82,6 +103,19 @@ public class eocv_test_tyma extends OpMode {
             // this - as the docs said \-.-/ - will convert the view from the camera into black & white
             Imgproc.cvtColor(input, grey, Imgproc.COLOR_RGB2GRAY);
 
+
+            // Draws a simple box around the middle 1/2 of the entire frame:
+            Imgproc.rectangle(
+                    input,
+                    new Point(
+                            input.cols()/4,
+                            input.rows()/4),
+                    new Point(
+                            input.cols()*(3f/4f),
+                            input.rows()*(3f/4f)),
+                    // lavender is the color of the rectangle :3
+                    new Scalar(230, 230, 250), 4);
+
             return input; // Return the image that will be displayed in the viewport
             // (In this case the input mat directly)
 
@@ -93,6 +127,29 @@ public class eocv_test_tyma extends OpMode {
             // This method is executed from the UI thread, so be careful to not
             // perform any sort heavy processing here! Your app might hang otherwise
 
+            toggleRecording = !toggleRecording;
+
+            if(toggleRecording) {
+                /*
+                 * This is all you need to do to start recording.
+                 */
+                webCam1.startRecordingPipeline(
+                        new PipelineRecordingParameters.Builder()
+                                .setBitrate(4, PipelineRecordingParameters.BitrateUnits.Mbps)
+                                .setEncoder(PipelineRecordingParameters.Encoder.H264)
+                                .setOutputFormat(PipelineRecordingParameters.OutputFormat.MPEG_4)
+                                .setFrameRate(30)
+                                .setPath(Environment.getExternalStorageDirectory().getPath())
+                                .build());
+            }
+            else
+            {
+                /*
+                 * Note: if you don't stop recording by yourself, it will be automatically
+                 * stopped for you at the end of your OpMode
+                 */
+                webCam1.stopRecordingPipeline();
+            }
 
         }
     }
