ae2ad3488e2413b218ee09f0f1bf975ce5dfd99a
tay_ma_alda <temyhey@gmail.com>
2024-01-02 17:05:59 +0100

    default teleop code file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopCodes/teleop2test.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.teleopCodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/* circle = b;
+    cross = a;
+    triangle = y;
+    square = x;
+    share = back;
+    options = start;
+    ps = guide;
+    the moving stick = left_stick_button/right_stick_button);
+*/
+
+@TeleOp
+public class teleop2test extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
