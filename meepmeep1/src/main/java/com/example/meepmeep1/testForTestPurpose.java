26377da65a974a73b936e2d355241d6d8dc14b67
tay_ma_alda <temyhey@gmail.com>
2024-01-07 22:18:36 +0100

    added the meepmeep module all over again bruh

---
+++ meepmeep1/src/main/java/com/example/meepmeep1/testForTestPurpose.java
@@ -0,0 +1,32 @@
+package com.example.meepmeep1;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class testForTestPurpose {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(500);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -70, 1.57))
+                                .forward(30)
+                                .waitSeconds(4)
+                                .turn(Math.toRadians(-90))
+                                .forward(35)
+                                .strafeLeft(5)
+                                .waitSeconds(6)
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
