ce225ef781cbe09e3d6a365258151ccfdeca4fa6
tay_ma_alda <temyhey@gmail.com>
2024-02-06 18:43:22 +0100

    blalblba

--- TeamCodeRR/src/main/java/org/firstinspires/ftc/teamcode/testingTrajectories/finalFarAutoTrajectoryTest1.java
+++ TeamCodeRR/src/main/java/org/firstinspires/ftc/teamcode/testingTrajectories/finalFarAutoTrajectoryTest1.java
@@ -80,6 +80,7 @@ public class finalFarAutoTrajectoryTest1 extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
         // webCam and pipeline init
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "webCam1");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
